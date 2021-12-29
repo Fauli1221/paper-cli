17bc91802fade05f7ba9e150bac8e4ce962ea292
Tch1b0 <Jpour3006@gmail.com>
2021-12-29 09:55:58 +0100

    fix imports

--- papercli/cli.py
+++ papercli/cli.py
@@ -1,16 +1,14 @@
 """import argparse sys urllib request and textgen and save"""
 import argparse
 import sys
-from time import sleep
-from typing import Any, Callable
-from rich.console import Console
+from typing import Callable
 import keyboard
 import os
 
 from papercli.paperapi import PaperApi, Build
+from papercli.utility import *
 
-console = Console()
-api: PaperApi = PaperApi()
+api = PaperApi()
 
 # CLI version
 VERSION = "0.3.0"
@@ -44,10 +42,10 @@ def cli_main():
 
     # Download build from flag information
     else:
-        arg_check(args, destination)
+        download_by_args(args, destination)
 
 
-def cli_gui(destination: str = "./", experimental: bool = False):
+def cli_gui(destination: str = None, experimental: bool = False):
     """
     The interactive cli PaperMC downloader
     """
@@ -65,11 +63,15 @@ def cli_gui(destination: str = "./", experimental: bool = False):
 
     with console.status("", spinner="dots"):
         project_ids = api.get_project_ids()
-    project = api.get_project(project_ids[select(project_ids, prompt="select a project")])
+    project = api.get_project(project_ids[
+        select(project_ids, prompt="select a project")
+        ])
 
     with console.status("", spinner="dots"):
         mc_version_groups = project.get_version_groups()
-    mc_version_group = mc_version_groups[select(mc_version_groups, prompt="select your minecraft version group")]
+    mc_version_group = mc_version_groups[
+        select(mc_version_groups, prompt="select your minecraft version group")
+        ]
 
     with console.status("", spinner="dots"):
         # Get all versions that start with the 
@@ -78,18 +80,41 @@ def cli_gui(destination: str = "./", experimental: bool = False):
             lambda x: x.startswith(mc_version_group),
             project.get_versions()    
         ))
-    mc_version = mc_versions[select(mc_versions, prompt="select your target minecraft version")]
+    mc_version = mc_versions[
+        select(mc_versions, prompt="select your target minecraft version")
+        ]
 
     with console.status("", spinner="dots"):
         builds = project.get_all_builds(mc_version)
     
     render = lambda choice, selected: choice.build if not selected else str(choice)
-    build = builds[select(builds, render=render, prompt="select a build (lowest is latest)")]
+    build = builds[
+        select(builds, render=render, prompt="select a build (lowest is latest)")
+        ]
+
+    if not destination:
+        dirs = get_current_dirs()
+        dest_dir = dirs[
+            select(dirs, prompt="where do you want to install the file?")
+        ]
+
+        if not experimental:
+            # Can't select a name using experimental mode,
+            # Because input is blocked in some way
+            name = console.input(
+                    "[bold magenta]=== how should the file be called? ===[/bold magenta]\n[grey23]leave empty for default name[/grey23]\nName: "
+                )
+            if len(name) == 0:
+                destination = dest_dir
+            else:
+                destination = os.path.join(dest_dir, name if name.endswith(".jar") else name + ".jar")
+        else:
+            destination = dest_dir
 
     if select(
         ["yes", "no"],
-        prompt=f"Do you want to download {project.id}, build {build.build} for MC version {build.version}?"
-       ) == 0:
+        prompt=f"Do you want to download {project.id}, build {build.build} for MC version {build.version} to {destination}?"
+        ) == 0:
         with console.status("[bold green]downloading",spinner="aesthetic"):
             build.download(destination)
         console.print(
@@ -100,74 +125,7 @@ def cli_gui(destination: str = "./", experimental: bool = False):
         console.print("[bold red]exiting...[/bold red]")
         sys.exit(0)
 
-
-def simple_user_select(choices: list[str], prompt: str = None, new_line: bool = True,render=None) -> int:
-    """
-    Displays the `choices` on the screen and
-    returns the index of the selected item
-    """
-    if prompt:
-        console.print(f"[bold magenta]=== {prompt} ===[/bold magenta]")
-
-    for i, choice in enumerate(choices):
-        console.print(f"[cyan]([b]{i + 1}[/b])[/cyan]: [white]{choice}[/white]")
-
-    index: int = None
-    while index is None or index > len(choices) - 1 or index < 0:
-        try:
-            index = int(console.input(f"Select (1-{len(choices)}): ")) - 1
-        except ValueError:
-            console.print("[red]please enter a valid number[/red]")
-    if new_line: print()
-    return index
-
-def fancy_user_select(
-    choices: list, 
-    render: Callable[[Any, bool], str] = lambda choice, _: choice, 
-    prompt: str = None
-    ) -> int:
-    selected = 0
-    
-    def print_state():
-        """
-        Print the current state of the selection
-        """
-        
-        # Clear the terminal screen
-        os.system("cls||clear")
-
-        if prompt:
-            console.print(f"[bold magenta]=== {prompt} ===[/bold magenta]")
-        console.print("[grey23]use 'w' and 's' for selection[/grey23]", end="\n\n")
-
-        for i, choice in enumerate(choices):
-            if i == selected:
-                console.print(f"[bold cyan]> {render(choice, True)}[/bold cyan]")
-            else:
-                console.print(f"[grey46]{render(choice, False)}[/grey46]")
-
-    print_state()
-
-    while True:
-        sleep(.1)
-        pressed_key = keyboard.read_key()
-
-        if pressed_key == "w": 
-            if selected > 0:
-                selected -= 1
-            else:
-                selected = len(choices) - 1 
-        elif pressed_key == "s":
-            if selected < len(choices) - 1:
-                selected += 1
-            else:
-                selected = 0
-        elif pressed_key == "enter":
-            return selected
-        
-        print_state()
-
-def arg_check(args: list[str], destination: str):
+def download_by_args(args: list[str], destination: str):
     """
     Validate args & download build
     """
@@ -183,10 +141,11 @@ def arg_check(args: list[str], destination: str):
 
     with console.status("[bold green]downloading...", spinner="aesthetic"):
         build.download(destination)
+
     console.print(
         f"[bold green]Successfully downloaded build {build.build}, version {build.version} to {destination}[/bold green]"
     )
 
 
 if __name__ == "__main__":
-    cli_gui()
+    cli_gui(None, True)
