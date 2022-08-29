5d5a8686d4734504a1b1cfbdfc57290101f426ba
Fauli1221 <paul.schugardt@gmail.com>
2022-08-29 22:55:54 +0200

    hopefully this fixes it

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name='paper-cli',
-    version='0.2.2',
+    version='0.2.3',
     packages=['papercli'],
+    long_description='tool to easyly get paper',
     install_requires=[
         'requests',
         'rich'
